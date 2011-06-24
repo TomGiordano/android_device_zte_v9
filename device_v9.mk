b14241841597adfd09c95183094bc217e8d074e4
kallt_kaffe <kallt_kaffe@apedroid.com>
2011-06-24 18:28:26 +0200

    Removed ldpi

--- device_v9.mk
+++ device_v9.mk
@@ -127,7 +127,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.hwrotation=270
 
 # V9 uses medium-density artwork where available
-PRODUCT_LOCALES += ldpi mdpi
+PRODUCT_LOCALES += mdpi
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
